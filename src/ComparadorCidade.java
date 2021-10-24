17965112b168c7be2bd3ab5b8b6e02f305369e3d
Danilo Lima <daniloleandro@gmail.com>
2021-10-24 18:08:53 -0300

    Inclusao de ajustes e comentários em: ComparadorCidade e ComparadorCidadeTest

--- src/ComparadorCidade.java
+++ src/ComparadorCidade.java
@@ -1,13 +1,22 @@
 import java.util.Comparator;
 
-/* Compare two cidades for use in sorting or data structures
- */ 
+/**
+ * Compare duas cidades para uso em classificação ou estruturas de dados
+ */
 public class ComparadorCidade implements Comparator<Cidade> {
-  /* compare cidades by their distancia from the start of the rail network
-   * return: negative if c1 is closer to 0, 0 if equal distancia, positive if c2 is closer to 0
-   */
-  public int compare(Cidade x, Cidade y) {
-    //return x.comparaDistancia(y,x);  //BUG03 Os parâmetros estão invertidos
-    return x.comparaDistancia(y);  //Correção do código e a refatoração para passar somente um parâmetro
+
+
+
+  /**
+   * Compara cidades pelo distancia  do ponto de início da rede ferroviaria
+   * return: inteiro negativo se cidade1 estiver mais perto da origem 0,
+   *         0 se distancias iguais,
+   *         inteiro positivo se cidade2 estiver mais perto da origem 0
+   */                                                      //Refatoração: Padronização do comentário
+  public int compare(Cidade cidade1, Cidade cidade2) {     //Refatoração: O nome do parâmetro foram alterados para cidade1 e cidade2
+    //return x.comparaDistancia(y,x);                      //BUG 03: Os parâmetros estão invertidos
+    return cidade1.comparaDistancia(cidade2);                          //Refatoração: Correção do BUS 03, através do ajuste dos parâmetros
+                                                           //Refatoração: ajustando a passagem de apenas um parâmetro
   }
+
 }
--- test/ComparadorCidadeTest.java
+++ test/ComparadorCidadeTest.java
@@ -8,19 +8,22 @@ class ComparadorCidadeTest {
     public Cidade cidade2 = new Cidade("Cidade 02");
     public ComparadorCidade comparadorCidades = new ComparadorCidade();
 
-    //Refactado o nome dos métodos para o padrão test<NomeDoMétodo>
+    //Refatoração: Os nomes dos métodos foram alterados para o padrão test<NomeDoMétodo>
+
     @Test
     void testCompareDistanciaPrimeiraCidadeMaisProxima() {
         cidade1.distancia=50;
         cidade2.distancia=100;
         assertEquals(-50,comparadorCidades.compare(cidade1,cidade2), "A primeira cidade não é a mais próxima a rede de trens");
     }
+
     @Test
     void testCompareDistanciaIgualDasCidadesParaRedeDeTrens() {
         cidade1.distancia=50;
         cidade2.distancia=50;
         assertEquals(0,comparadorCidades.compare(cidade1,cidade2), "As cidades não possuem distâncias iguais em relação a rede de trens");
     }
+
     @Test
     void testCompareDistanciaSegundaCidadeMaisProxima() {
         cidade1.distancia=100;
